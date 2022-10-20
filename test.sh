21cb48ea5d0073e777adbdf813f9dc22357fbef3
Gabriel <gabriel.desseresusini@littleworker.fr>
2022-10-20 09:30:23 +0200

    update test.sh and ci

--- test.sh
+++ test.sh
@@ -3,9 +3,11 @@
 originalbranch=$@
 commandoutput=$(git branch -r --merged origin/develop $originalbranch)
 
-if [ $commandoutput != "origin/feature/fifth-feature" ]; then
+if [ "$commandoutput" != "origin/feature/fifth-feature" ]; then
+  echo "PR not allowed"
   exit 1
 #  refuse PR
 else
+  echo "PR allowed"
   exit 0
 fi
