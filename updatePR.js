6421a3778ff0eee94bbcefb6a6f1b15b856e3da5
Gabriel <gabriel.desseresusini@littleworker.fr>
2022-10-28 16:23:55 +0200

    fix: update main

--- updatePR.js
+++ updatePR.js
@@ -1,25 +1,24 @@
-const github = require('@actions/github');
+(async () => {
+    const github = require('@actions/github');
 
-const token = process.env.GITHUB_TOKEN
+    const token = process.env.GITHUB_TOKEN
+    const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
-const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
+    const prNum = github.context.payload.pull_request.number;
 
-const prNum = github.context.payload.pull_request.number;
-
-const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token);
 
 
-const { data } = await octokit.rest.pulls.get({
-    owner: repoOwner,
-    repo: repoName,
-    pull_number: prNum,
-});
+    const {data} = await octokit.rest.pulls.get({
+        owner: repoOwner,
+        repo: repoName,
+        pull_number: prNum,
+    });
 
-console.log(data)
-
-octokit.pulls.update({
-    owner: repoOwner,
-    repo: repoName,
-    body: "walou", // get current body + new body
-    pull_number: prNum,
-});
+    octokit.rest.pulls.update({
+        owner: repoOwner,
+        repo: repoName,
+        body: data.body + "\n\n verify PR's status",
+        pull_number: prNum,
+    });
+})();
