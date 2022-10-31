18164c10c314a7ae74e6e02cc1ec3afd9f94bcbd
Gabriel <gabriel.desseresusini@littleworker.fr>
2022-10-31 10:14:54 +0100

    fix: update main

--- updatePR.js
+++ updatePR.js
@@ -3,7 +3,7 @@
     prId = process.argv[1]
     const github = require('@actions/github');
 
-    const token = process.env.GITHUB_TOKEN
+    const token = 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
     const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
     // const prNum = github.context.payload.pull_request.number;
