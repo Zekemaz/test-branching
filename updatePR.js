9aeca5d0728a518fea7f3fa4ac16ff619bb2dcbb
Gabriel <gabriel.desseresusini@littleworker.fr>
2022-10-31 09:43:01 +0100

    fix: update nein

--- updatePR.js
+++ updatePR.js
@@ -1,10 +1,12 @@
 (async () => {
+
+    prId = process.argv[1]
     const github = require('@actions/github');
 
     const token = process.env.GITHUB_TOKEN
     const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
-    const prNum = github.context.payload.pull_request.number;
+    // const prNum = github.context.payload.pull_request.number;
 
     const octokit = github.getOctokit(token);
 
@@ -12,13 +14,13 @@
     const {data} = await octokit.rest.pulls.get({
         owner: repoOwner,
         repo: repoName,
-        pull_number: prNum,
+        pull_number: prId,
     });
 
     octokit.rest.pulls.update({
         owner: repoOwner,
         repo: repoName,
         body: data.body + "\n\n verify PR's status",
-        pull_number: prNum,
+        pull_number: prId,
     });
 })();
