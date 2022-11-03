94ba5dc267829d13b26dde2aa05251294654131e
Gabriel <gabriel.desseresusini@littleworker.fr>
2022-11-03 14:49:07 +0100

    fix: update main

--- delete_artifact.js
+++ delete_artifact.js
@@ -1,8 +1,12 @@
 (async () => {
 
+    const token = process.argv[1]
+    console.log(token)
+
     const github = require('@actions/github');
     const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
-    const octokit = github.getOctokit(process.argv[1]);
+    const octokit = github.getOctokit(token);
+
 
     // list all artifacts
     const {artifacts} = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
@@ -11,6 +15,7 @@
     })
 
     console.log(artifacts)
+    console.log(artifacts.artifacts[1].id)
 
     for (let i = 0; i < artifacts.total_count; i++) {
         await octokit.request('DELETE /repos/{owner}/{repo}/actions/artifacts/{artifact_id}', {
